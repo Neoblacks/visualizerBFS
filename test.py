f2e97e8167a59bdaef3acce7faa1216fac55659a
Neoblacks <antoine.mugnier035@gmail.com>
2023-02-12 12:33:11 +0100

    Add nb of steps

---
+++ test.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from time import sleep
+
+def create_window():
+    root = tk.Tk()
+    root.title("Test")
+    root.display()
--- v2.py
+++ v2.py
@@ -42,6 +42,13 @@ import tkinter as tk
 from time import sleep
 import re
 
+def close_window():
+    # destroy label and canvas
+    label.destroy()
+    canvas.destroy()
+
+    root.destroy()
+
 def display_grid(canvas, grid, seen):
     canvas.delete("all")
     for y, row in enumerate(grid):
@@ -54,27 +61,35 @@ def display_grid(canvas, grid, seen):
                 color = "royalblue"
             elif grid[y][x] == 'C':
                 color = "goldenrod"
+                # label_collec.update()
             else:
                 color = "forestgreen"
             canvas.create_rectangle(x*40, y*40, x*40+40, y*40+40, fill=color)
             canvas.create_text(x*40+20, y*40+20, text=cell, font="Arial 20 bold", fill= "white" if color=="black" else "black")
-
 root = tk.Tk()
 root.title("BFS Visualizer")
 grid = [list(line) for line in open("map.ber").read().strip().split("\n")]
 lines = open("pos.txt").read().strip().split("\n")
 seen = set()
-canvas = tk.Canvas(root, width=len(grid[0])*40, height=len(grid)*40)
+canvas = tk.Canvas(root, width=len(grid[0])*40, height=len(grid)*40 + 100)
 canvas.pack()
+label = tk.Label(root, text = "Number of steps: ", font = "Arial 20 bold", fg = "black", bg = "lightgrey")
+label_collec = tk.Label(root, text = "Number of Collect found: ", font = "Arial 20 bold", fg = "black", bg = "lightgrey")
+label.place(x = 0, y = len(grid)*40 + 5)
+label_collec.place(x = 300, y = len(grid)*40 + 5)
 for i, line in enumerate(lines):
+    label.config(text = "Number of steps: " + str(i), font = "Arial 20 bold", fg = "black", bg = "lightgrey")
+    # display number of collectibles found and increment it when C is found
+    # label_collec.config(text = "Number of Collectibles found: " + str(line.count('C')), font = "Arial 20 bold", fg = "black", bg = "lightgrey")
+    label.update()
     bx, by, bc = re.match(r"x: (\d+), y: (\d+), C: (.)", line).groups()
     bx, by = int(bx), int(by)
     seen.add((bx, by))
     if i%5==0 or i==len(lines)-1:
-        display_grid(canvas, grid, seen)
         # sleep(0.03)
         root.update()
+        display_grid(canvas, grid, seen)
+# display_grid(canvas, grid, seen)
 
-
+root.bind('<Escape>', lambda e: close_window())
 root.mainloop()
-
